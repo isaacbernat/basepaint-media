15b7b40c53509bdef7f33a77263b1a7f302c8a07
Isaac Bernat <isaac.bernat@gmail.com>
2025-04-03 12:58:33 +0200

    Updates to match code repo for video_to_images changes

--- create_archive.py
+++ create_archive.py
@@ -5,6 +5,8 @@ from image_to_pdf import create_pdf
 LATEST = 560
 BATCH_SIZE = 100
 CREATE_COVER = True
+INCLUDE_VIDEO = True
+ARCHIVE_VERSION = "0.2.0"
 
 
 if __name__ == '__main__':
@@ -13,4 +15,4 @@ if __name__ == '__main__':
     create_metadata_csv(LATEST)
     enrich_metadata_csv()
     fetch_files(LATEST, "videos")
-    create_pdf(BATCH_SIZE, CREATE_COVER)
+    create_pdf(BATCH_SIZE, CREATE_COVER, INCLUDE_VIDEO)
